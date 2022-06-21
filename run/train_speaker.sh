398f3e45458c2afba21e727567509423302123f1
chenjinyubuaa <102395736+chenjinyubuaa@users.noreply.github.com>
2022-06-21 23:33:17 +0800

    Update train_speaker.sh

--- run/train_speaker.sh
+++ run/train_speaker.sh
@@ -4,4 +4,4 @@ flag="--attn soft --train speaker
       --speaker methods/SEvol/snaps/speaker/best_val_unseen_bleu
       --subout max --dropout 0.6 --optim adam --lr 1e-4 --iters 80000 --maxAction 35"
 mkdir -p snap/$name
-CUDA_VISIBLE_DEVICES=$1 python3 methods/nvem/train.py $flag --name $name 
+CUDA_VISIBLE_DEVICES=$1 python3 methods/SEvol/train.py $flag --name $name 
