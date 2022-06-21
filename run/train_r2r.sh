36269530c97d3f9b136e0d713a15c4d9968b23c9
chenjinyubuaa <102395736+chenjinyubuaa@users.noreply.github.com>
2022-06-21 23:32:57 +0800

    Update train_r2r.sh

--- run/train_r2r.sh
+++ run/train_r2r.sh
@@ -7,7 +7,7 @@ flag="--attn soft --train listener
       --gcn_topk 5 --glove_dim 300 --top_N_obj 8 --distance_decay_function same  
       --subout max --dropout 0.5 --optim rms --lr 1e-4 --iters 80000 --maxAction 20"
 mkdir -p snap/$name
-CUDA_VISIBLE_DEVICES=$1 python3 methods/nvem/train.py $flag --name $name 
+CUDA_VISIBLE_DEVICES=$1 python3 methods/Sevol/train.py $flag --name $name 
 
 # Try this with file logging:
 # CUDA_VISIBLE_DEVICES=$1 unbuffer python r2r_src/train.py $flag --name $name | tee snap/$name/log
